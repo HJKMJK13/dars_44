3adec1935e50e0794820f18256e58c52642ab824
HJKMJK13 <norboyevkarimjon06@gmail.com>
2023-05-09 13:35:03 +0500

    innerHTML boshlandi(started)

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-const title=document.getElementById("title")
-title.textContent +=" New text"
-console.log(title.textContent)
+// const title=document.getElementById("title")
+// title.textContent +=" New text"
+// console.log(title.textContent)
 
-const listItem=document.querySelectorAll(".list-item")
-listItem.forEach((item)=>{
-    item.textContent +=" darslari"
-    console.log(item.textContent)
-})
+// const listItem=document.querySelectorAll(".list-item")
+// listItem.forEach((item)=>{
+//     item.textContent +=" darslari"
+//     console.log(item.textContent)
+// })
+
+
+//innerHTML
+const title=document.getElementById("title")
