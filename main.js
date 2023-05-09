9d6b0480f1a6693db017bbf218d85ebef019b81d
HJKMJK13 <norboyevkarimjon06@gmail.com>
2023-05-09 13:30:13 +0500

    li ichida yozilgan textlarga darslari degan matnni qo'shib qoydik

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 const title=document.getElementById("title")
-console.log(title)
+title.textContent +=" New text"
+console.log(title.textContent)
+
+const listItem=document.querySelectorAll(".list-item")
+listItem.forEach((item)=>{
+    item.textContent +=" darslari"
+    console.log(item.textContent)
+})
