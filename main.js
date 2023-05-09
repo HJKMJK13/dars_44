35ea376593024d3a7d6f143a87be29fd49eabaa4
HJKMJK13 <norboyevkarimjon06@gmail.com>
2023-05-09 13:56:38 +0500

    innerHTML bilan TextContentning farqi haqida malumotlar berildi

--- main.js
+++ main.js
@@ -10,4 +10,23 @@
 
 
 //innerHTML
-const title=document.getElementById("title")
+// const title=document.getElementById("title")
+// title.innerHTML ="<i>new text html</i>"
+
+// const names=["Dilshod","Ilhom","Karimjon"]
+// const ol=document.querySelector("ol")
+// names.forEach((name)=>{
+//     ol.innerHTML +=`<li>${name}</li>`
+// })
+
+// const names=["Karimjon","Dilshod","Burjoq"]
+// const ol=document.querySelector("ol")
+// names.forEach((name)=>{
+//     ol.innerHTML +=`<li>${name}</li>`
+// })
+
+const names=["Karimjon","Dilshod","Burjoq"]
+const ol=document.querySelector("ol")
+names.forEach((name)=>{
+    ol.textContent +=`${name}`
+})
